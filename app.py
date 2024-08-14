044a8e3a74cbf35edf75d3cd1035596d8b139c46
Abel731 <abelp9289@gmail.com>
2024-08-13 20:38:11 -0400

    recuperar datos del servidor que envio el navegador

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,7 +14,13 @@ def contacto():
 def contacto2():
     return render_template('contacto.html')
 
+@app.route('/guardar-mascota', methods=['POST'])
+def guardarMascota():
+    print(request.form)
+    nombreMascota = request.form.get('txtNamemascota')
+    razaMascota = request.form.get('txtRaza')
+    return f"Ya llego tu mascota {nombreMascota}, la raza es {razaMascota} al server"
 
-# se pregunta por el proceso principal
+# se pregunta por el proceso principalla
 if __name__=='__main__':
     app.run(debug=True)
